098239e9b5152cb65bd31d12c2e1f6c60ee8d5a8
maartje <maartjedejonge@gmail.com>
2018-05-24 21:57:37 +0200

    use epochs in training

--- src/rnn_model_train.py
+++ src/rnn_model_train.py
@@ -70,7 +70,7 @@ def train(input_tensor, target_tensor, encoder, decoder,
     return loss.item() / target_length
 
 
-def trainIters(index_array_pairs, encoder, decoder, n_iters, max_length,
+def trainIters(index_array_pairs, encoder, decoder, n_epochs, max_length,
         print_every=1000, plot_every=100, learning_rate=0.01, max_hours = 24, clip = None):
     start = time.time()
     start_time = datetime.now()
@@ -90,47 +90,49 @@ def trainIters(index_array_pairs, encoder, decoder, n_iters, max_length,
     plot_loss_total = 0  # Reset every plot_every
 
     optimizer = optim.SGD(chain(encoder.parameters(), decoder.parameters()), lr=learning_rate)
-    training_pairs = [random.choice(tensor_pairs) for i in range(n_iters)]
     criterion = nn.NLLLoss()
     loss_peak = False
+    
+    n_iters = n_epochs * len(tensor_pairs)
+    for epoch in range(n_epochs):
 
-    for iter in range(1, n_iters + 1):
-        training_pair = training_pairs[iter - 1]
-        input_tensor = training_pair[0]
-        target_tensor = training_pair[1]
+        for i in range(0, len(tensor_pairs)):
+            iter = i + 1 + epoch * len(tensor_pairs)
+            training_pair = tensor_pairs[i]
+            input_tensor = training_pair[0]
+            target_tensor = training_pair[1]
 
-        loss = train(input_tensor, target_tensor, encoder,
-                     decoder, optimizer, criterion, max_length, clip)
-        
-        # MJ: debug peaks in loss diagram
-        if not loss_peak and plot_losses and (loss > 2.5 * plot_losses[-1]) : 
-            loss_peak = True
-            print ('peak in loss diagram: ')
-            print ('iter', iter)
-            print (' current loss', loss)
-            print ('avg prev loss', plot_losses[-1])
-            print (input_tensor)
-            print (target_tensor)
-            print ()
+            loss = train(input_tensor, target_tensor, encoder,
+                         decoder, optimizer, criterion, max_length, clip)
 
-        print_loss_total += loss
-        plot_loss_total += loss
+            # MJ: debug peaks in loss diagram
+            if not loss_peak and plot_losses and (loss > 2.5 * plot_losses[-1]) : 
+                loss_peak = True
+                print ('peak in loss diagram: ')
+                print (' current loss', loss)
+                print ('avg prev loss', plot_losses[-1])
+                print ('epoch', epoch)
+                print ('sentence index', i)
+                print ()
 
-        if iter % print_every == 0:
-            print_loss_avg = print_loss_total / print_every
-            print_loss_total = 0
-            print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
-                                         iter, iter / n_iters * 100, print_loss_avg))
+            print_loss_total += loss
+            plot_loss_total += loss
 
-        if iter % plot_every == 0:
-            plot_loss_avg = plot_loss_total / plot_every
-            plot_losses.append(plot_loss_avg)
-            plot_loss_total = 0
-            loss_peak = False
+            if iter % print_every == 0:
+                print_loss_avg = print_loss_total / print_every
+                print_loss_total = 0
+                print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters),
+                                             iter, iter / n_iters * 100, print_loss_avg))
 
-        if datetime.now() > end_time:
-            print(f'exceeded max hours {max_hours}')
-            break
+            if iter % plot_every == 0:
+                plot_loss_avg = plot_loss_total / plot_every
+                plot_losses.append(plot_loss_avg)
+                plot_loss_total = 0
+                loss_peak = False
+
+            if datetime.now() > end_time:
+                print(f'exceeded max hours {max_hours}')
+                break
 
     return plot_losses
 

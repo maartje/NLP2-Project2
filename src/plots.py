2981d530e086e4d94f5f94a36c5fbfd71da86f57
maartje <maartjedejonge@gmail.com>
2018-05-24 17:08:35 +0200

    print sentences with @@

--- src/plots.py
+++ src/plots.py
@@ -16,6 +16,13 @@ def showLosses(plot_losses, plot_every):
 
 
 def showAttention(input_words, output_words, attentions, merge_subwords = False):
+    input_sentence = ' '.join(input_words)
+    output_sentence = ' '.join(output_words)
+    print (input_sentence)
+    print (input_sentence.replace('@@ ', ''))
+    print (output_sentence)
+    print (output_sentence.replace('@@ ', ''))
+
     if merge_subwords:
         (input_words, output_words, attentions) = merge_bpe(input_words, output_words, attentions)
 
@@ -35,12 +42,6 @@ def showAttention(input_words, output_words, attentions, merge_subwords = False)
 
     plt.show()
 
-    input_sentence = ' '.join(input_words)
-    output_sentence = ' '.join(output_words)
-    print (input_sentence)
-    print (input_sentence.replace('@@ ', ''))
-    print (output_sentence)
-    print (output_sentence.replace('@@ ', ''))
 
 def merge_bpe(s_words, t_words, A):
     s_words_merged, X = merge_bpe_s(s_words, A)
